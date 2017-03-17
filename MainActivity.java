4eae1a744debf59d6b6b2f591460e8a681adc048
Alex <877715168.qq.com>
2017-03-17 15:42:06 +0800

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.statusbar_alexleo.libdependenciesdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
